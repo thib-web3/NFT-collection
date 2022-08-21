cf12f45ec303c0b703724558b2f318fade39293d
thiblehm12 <thiblehm12@gmail.com>
2022-08-21 11:33:55 +0200

    first commit

---
+++ contracts/NFT-collec-contracts.sol
@@ -0,0 +1,56 @@
+// SPDX-License_Identifier: minutes
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTContract is ERC721, Ownable {
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+
+    Counters.Counter private supply;
+
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+    string public hiddenMetadataUri;
+
+    uint256 public cost = 0.01;
+    uint256 public maxSupply = 100;
+    uint256 public maxMintAmountPerTx = 5;
+
+    bool public paused = true;
+    bool public revealed = false;
+
+
+}
+
+function setUriPrefix(string memory _uriPrefix) public onlyOwner{
+    uriPrefix = _uriPrefix;
+}
+
+function setUriSuffix(string memory _uriSuffix) public onlyOwner{
+    uriSuffix = _uriSuffix;
+}
+function setHiddenMetadataUri(string memory _setHiddenMetadataUri) public onlyOwner{
+    hiddenMetadataUri = _hiddenMetadataUri;
+}
+
+function setCost(uint256 _cost) public onlyOwner{
+    cost = _cost;
+}
+
+function setMaxSupply(uint256 _maxSupply) public onlyOwner{
+    maxSupply = _maxSupply;
+}
+
+function setMaxMintAmountPerTx( uint256 _maxMintAmountPerTx) public onlyOwner{
+    maxMintAmountPerTx =_maxMintAmountPerTx;
+}
+
+function setPaused(bool _state) public onlyOwner{
+    paused = _paused
+}
+function setRevealed(bool _state) public onlyOwner{
+    revealed = _revealed;
+}
